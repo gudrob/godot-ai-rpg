8b5674cb63276dbdedfa7ebb72f0959a78f8911e
Robert Gudat <robert.gudat@web.de>
2023-11-27 13:31:23 +0100

    Added metal driver, added mac execution logic.

--- Test.cs
+++ Test.cs
@@ -1,66 +1,58 @@
-using LLama.Common;
-using LLama;
-using System.Collections.Generic;
+using System;
+using System.Diagnostics;
 using Godot;
 
 public partial class Test : Node
 {
 	public string prompt = null;
 
+	public string text = "";
+
 
 	public async override void _Ready()
 	{
-		string modelPath = "/Users/robertgudat/godot-ai-rpg/model/model.gguf"; // change it to your own model path
-		prompt = @"<s>[INST] <<SYS>>
-You voice video game characters.
-Information about the character you voice will be provided between the tags <CHAR> and </CHAR>.
-The character will be told something between the tags <INPUT> and </INPUT>.
-You will write what the character says between the tags <OUTPUT> and </OUTPUT>
-and the emotion the character feels between the tags <EMO> and </EMO>.
-Possible emotions are happy, angry, sad, neutral and surprised.
-Example output: <OUTPUT> Hello, how are you? </OUTPUT> <EMO> happy </EMO>.
-<</SYS>>
+		string modelPath = "/Users/-/godot-ai-rpg/model/model.gguf"; // change it to your own model path
+		prompt = $"Loading model ${modelPath}";
 
-<CHAR> You are a humble blacksmith from a village close to a river. You are 25 years old. </CHAR> <INPUT> What is 10 times 10? </INPUT> [/INST] ";
+		var label = FindChild("Label", true) as Label;
 
-		// Load model
-		var parameters = new ModelParams(modelPath)
+		// Start the child process.
+		Process process = new();
+		// Redirect the output stream of the child process.
+		process.StartInfo.ErrorDialog = false;
+		process.StartInfo.UseShellExecute = false;
+		process.EnableRaisingEvents = true;
+		process.StartInfo.CreateNoWindow = true;
+		process.StartInfo.RedirectStandardError = true;
+		process.StartInfo.RedirectStandardInput = true;
+		process.StartInfo.RedirectStandardOutput = true;
+		process.StartInfo.WorkingDirectory = "/Users/-/godot-ai-rpg/apple-silicon-llama/";
+		process.StartInfo.FileName = "/Users/-/godot-ai-rpg/apple-silicon-llama/main";
+		process.StartInfo.Arguments = $"-m \"{modelPath}\"";
+		process.OutputDataReceived += process_OutputDataReceived;
+		process.ErrorDataReceived += process_ErrorDataReceived;
+		process.Exited += process_Exited;
+		GD.Print("AI Process Start Result: " + process.Start());
+
+		process.BeginErrorReadLine();
+		process.BeginOutputReadLine();
+
+		void process_Exited(object sender, EventArgs e)
 		{
-			ContextSize = 4096,
-			Threads = 1,
-		};
-		using var model = LLamaWeights.LoadFromFile(parameters);
+			text += process.ExitCode + System.Environment.NewLine;
+			label.SetDeferred("text", text);
+		}
 
-		// Initialize a chat session
-		using var context = model.CreateContext(parameters);
-		var ex = new InteractiveExecutor(context);
-		ChatSession session = new ChatSession(ex);
-
-		// run the inference in a loop to chat with LLM
-		while (true)
+		void process_OutputDataReceived(object sender, DataReceivedEventArgs e)
 		{
-			if(prompt != null)
-			{
-				var sw = System.Diagnostics.Stopwatch.StartNew();
+			text += e.Data + System.Environment.NewLine;
+			label.SetDeferred("text", text);
+		}
 
-				var texts = session.ChatAsync(prompt, new InferenceParams() { Temperature = 0.5f, AntiPrompts = new List<string> { "[END]" } });
-
-				GD.Print(prompt);
-
-				var answer = "";
-
-				await foreach (var text in texts)
-				{
-					answer += text;
-					await ToSignal(GetTree(), SceneTree.SignalName.ProcessFrame);
-				}
-
-				GD.Print(answer);
-				GD.Print("Time taken (ms): "+sw.Elapsed.TotalMilliseconds);
-
-				prompt = "</s><s>[INST] <INPUT> Have you heard any rumors? </INPUT> [/INST]";
-			}
-			await ToSignal(GetTree(), SceneTree.SignalName.ProcessFrame);
+		void process_ErrorDataReceived(object sender, DataReceivedEventArgs e)
+		{
+			text += e.Data + System.Environment.NewLine;
+			label.SetDeferred("text", text);
 		}
 	}
 }

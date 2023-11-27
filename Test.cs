08811b65b94889290498da5116a7c1262b68c6c4
Robert Gudat <robert.gudat@web.de>
2023-11-27 09:40:44 +0100

    Removed llamaSharp. Added apple silicon llama.cpp binaries

--- Test.cs
+++ Test.cs
@@ -10,13 +10,23 @@ public partial class Test : Node
 
 	public async override void _Ready()
 	{
-		string modelPath = "model"; // change it to your own model path
-		prompt = "<s>[INST] <<SYS>>You are a digital assistant. You answer immediately, precise and as short as possible <</SYS>> What is 10 times 10? [/INST] ";
+		string modelPath = "/Users/robertgudat/godot-ai-rpg/model/model.gguf"; // change it to your own model path
+		prompt = @"<s>[INST] <<SYS>>
+You voice video game characters.
+Information about the character you voice will be provided between the tags <CHAR> and </CHAR>.
+The character will be told something between the tags <INPUT> and </INPUT>.
+You will write what the character says between the tags <OUTPUT> and </OUTPUT>
+and the emotion the character feels between the tags <EMO> and </EMO>.
+Possible emotions are happy, angry, sad, neutral and surprised.
+Example output: <OUTPUT> Hello, how are you? </OUTPUT> <EMO> happy </EMO>.
+<</SYS>>
+
+<CHAR> You are a humble blacksmith from a village close to a river. You are 25 years old. </CHAR> <INPUT> What is 10 times 10? </INPUT> [/INST] ";
 
 		// Load model
 		var parameters = new ModelParams(modelPath)
 		{
-			ContextSize = 8192,
+			ContextSize = 4096,
 			Threads = 1,
 		};
 		using var model = LLamaWeights.LoadFromFile(parameters);
@@ -33,7 +43,7 @@ public partial class Test : Node
 			{
 				var sw = System.Diagnostics.Stopwatch.StartNew();
 
-				var texts = session.ChatAsync(prompt, new InferenceParams() { Temperature = 0.1f, AntiPrompts = new List<string> { "[END]" } });
+				var texts = session.ChatAsync(prompt, new InferenceParams() { Temperature = 0.5f, AntiPrompts = new List<string> { "[END]" } });
 
 				GD.Print(prompt);
 
@@ -48,7 +58,7 @@ public partial class Test : Node
 				GD.Print(answer);
 				GD.Print("Time taken (ms): "+sw.Elapsed.TotalMilliseconds);
 
-				prompt = "</s><s>[INST] What is 10 times 10 ? [/INST]";
+				prompt = "</s><s>[INST] <INPUT> Have you heard any rumors? </INPUT> [/INST]";
 			}
 			await ToSignal(GetTree(), SceneTree.SignalName.ProcessFrame);
 		}

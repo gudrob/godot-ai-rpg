7be4dc339aa30741c4e5dedbadc5febf650c3c78
Robert Gudat <robert.gudat@web.de>
2023-12-04 13:58:51 +0100

    Fixed macos path

--- LLaMA2.cs
+++ LLaMA2.cs
@@ -11,198 +11,200 @@ using Godot.Collections;
 
 namespace AIRPG
 {
-    public partial class LLaMA2 : Node
-    {
-        private static LLaMA2 Instance;
+	public partial class LLaMA2 : Node
+	{
+		private static LLaMA2 Instance;
 
-        private static Process LLaMAProcess;
+		private static Process LLaMAProcess;
 
-        private string completionAddress;
+		private string completionAddress;
 
-        private HttpService httpService;
+		private HttpService httpService;
 
-        public static async Task<string> Prompt(Session session, string prompt, int predictTokens = 192, float repeatPenalty = 1.1764705882352942f, float temperature = 0.25f)
-        {
-            var aiCharacterToken = $"{session.aiCharacterName}:";
-            var playerCharacterToken = $"{session.playerCharacterName}:";
+		public static async Task<string> Prompt(Session session, string prompt, int predictTokens = 192, float repeatPenalty = 1.1764705882352942f, float temperature = 0.25f)
+		{
+			var aiCharacterToken = $"{session.aiCharacterName}:";
+			var playerCharacterToken = $"{session.playerCharacterName}:";
 
-            session.fullPrompt
-                .Append(System.Environment.NewLine)
-                .Append(playerCharacterToken)
-                .Append(' ');
+			session.fullPrompt
+				.Append(System.Environment.NewLine)
+				.Append(playerCharacterToken)
+				.Append(' ');
 
-            session.fullPrompt
-                .Append(prompt)
-                .Append(System.Environment.NewLine)
-                .Append(aiCharacterToken)
-                .Append(' ');
+			session.fullPrompt
+				.Append(prompt)
+				.Append(System.Environment.NewLine)
+				.Append(aiCharacterToken)
+				.Append(' ');
 
-            var body = "{"
-            + $"\"temperature\": {temperature},"
-            + $"\"repeat_penalty\": {repeatPenalty},"
-            + $"\"n_predict\": {predictTokens},"
-            + $"\"cache_promt\": true,"
-            + $"\"prompt\": \"{HttpUtility.JavaScriptStringEncode(session.fullPrompt.ToString())}\","
-            + $"\"slot_id\": -1,"
-            + $"\"stream\": false,"
-            + $"\"stop\": [\"</s>\", \"{aiCharacterToken}\", \"{playerCharacterToken}\"]"
-            + "}";
+			var body = "{"
+			+ $"\"temperature\": {temperature},"
+			+ $"\"repeat_penalty\": {repeatPenalty},"
+			+ $"\"n_predict\": {predictTokens},"
+			+ $"\"cache_prompt\": true,"
+			+ $"\"prompt\": \"{HttpUtility.JavaScriptStringEncode(session.fullPrompt.ToString())}\","
+			+ $"\"slot_id\": -1,"
+			+ $"\"stream\": false,"
+			+ $"\"stop\": [\"</s>\", \"{aiCharacterToken}\", \"{playerCharacterToken}\"]"
+			+ "}";
 
-            var response = await Instance.httpService.PostAsync(Instance.completionAddress, body, "application/json");
+			GD.Print("REQUEST: " + body);
 
-            GD.Print(response);
+			var response = await Instance.httpService.PostAsync(Instance.completionAddress, body, "application/json");
 
-            var responseDict = (Dictionary<string, Variant>)Json.ParseString(response);
+			GD.Print("RESPONSE: " + response);
 
-            var res = responseDict["content"].ToString();
+			var responseDict = (Dictionary<string, Variant>)Json.ParseString(response);
 
-            session.fullPrompt.Append(res);
+			var res = responseDict["content"].ToString();
 
-            return res;
-        }
+			session.fullPrompt.Append(res);
 
-        public static Session StartSession(string aiCharacterName, string playerCharacterName, string basePrompt)
-        {
-            return new Session()
-            {
-                aiCharacterName = aiCharacterName,
-                playerCharacterName = playerCharacterName,
-                fullPrompt = new(basePrompt)
-            };
+			return res;
+		}
 
-        }
+		public static Session StartSession(string aiCharacterName, string playerCharacterName, string basePrompt)
+		{
+			return new Session()
+			{
+				aiCharacterName = aiCharacterName,
+				playerCharacterName = playerCharacterName,
+				fullPrompt = new(basePrompt)
+			};
 
-        public override void _Ready()
-        {
-            if (Instance != null) return;
+		}
 
-            Instance = this;
+		public override void _Ready()
+		{
+			if (Instance != null) return;
 
-            httpService = new();
+			Instance = this;
 
-            Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture("en-US");
+			httpService = new();
 
-            foreach (var process in Process.GetProcessesByName("server"))
-            {
-                process.Kill();
-            }
-        }
+			Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture("en-US");
 
-        public static void Initialize(int gpuLayers = 33, int cpuThreads = 2, int maximumSessions = 2, string host = "127.0.0.1", short port = 8080, int contextSize = 1024, int maxWaitTime = 600, bool allowMemoryMap = true, bool alwaysKeepInMemory = true)
-        {
-            Instance.InitializeServer(gpuLayers, cpuThreads, maximumSessions, host, port, contextSize, maxWaitTime, allowMemoryMap, alwaysKeepInMemory);
-        }
+			foreach (var process in Process.GetProcessesByName("server"))
+			{
+				process.Kill();
+			}
+		}
 
-        private void InitializeServer(int gpuLayers, int cpuThreads, int maximumSessions, string host, short port, int contextSize, int maxWaitTime, bool allowMemoryMapping, bool alwaysKeepInMemory)
-        {
-            var architecture = RuntimeInformation.ProcessArchitecture;
+		public static void Initialize(int gpuLayers = 33, int cpuThreads = 2, int maximumSessions = 2, string host = "127.0.0.1", short port = 8080, int contextSize = 1024, int maxWaitTime = 600, bool allowMemoryMap = true, bool alwaysKeepInMemory = true)
+		{
+			Instance.InitializeServer(gpuLayers, cpuThreads, maximumSessions, host, port, contextSize, maxWaitTime, allowMemoryMap, alwaysKeepInMemory);
+		}
 
-            string modelPath = "./../model/model.gguf";
-            string workingDirectory;
+		private void InitializeServer(int gpuLayers, int cpuThreads, int maximumSessions, string host, short port, int contextSize, int maxWaitTime, bool allowMemoryMapping, bool alwaysKeepInMemory)
+		{
+			var architecture = RuntimeInformation.ProcessArchitecture;
 
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
-            {
-                if (architecture == Architecture.Arm64)
-                {
-                    GD.Print("Detected MacOS on Arm64");
-                    workingDirectory = "./apple-silicon-llama/";
-                }
-                else
-                {
-                    GD.Print("MacOS on " + architecture.ToString() + " is currently unsupported");
-                    GetTree().Quit();
-                    return;
-                }
-            }
-            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-            {
-                if (architecture == Architecture.X64)
-                {
-                    GD.Print("Detected Windows on X64");
-                    workingDirectory = "./win-x64-llama/";
-                }
-                else
-                {
-                    GD.Print("Windows on " + architecture.ToString() + " is currently unsupported");
-                    GetTree().Quit();
-                    return;
-                }
-            }
-            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-            {
-                GD.Print("Linux is currently unsupported");
-                GetTree().Quit();
-                return;
-            }
-            else if (RuntimeInformation.IsOSPlatform(OSPlatform.FreeBSD))
-            {
-                GD.Print("FreeBSD is currently unsupported");
-                GetTree().Quit();
-                return;
-            }
-            else
-            {
-                GD.Print("Your Operating System is unsupported");
-                GetTree().Quit();
-                return;
-            }
+			string modelPath = "./../model/model.gguf";
+			string workingDirectory;
 
-            StartServer(modelPath, workingDirectory, gpuLayers, cpuThreads, maximumSessions, host, port, contextSize, maxWaitTime, allowMemoryMapping, alwaysKeepInMemory);
+			if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+			{
+				if (architecture == Architecture.Arm64)
+				{
+					GD.Print("Detected MacOS on Arm64");
+					workingDirectory = "./macos-arm64-llama/";
+				}
+				else
+				{
+					GD.Print("MacOS on " + architecture.ToString() + " is currently unsupported");
+					GetTree().Quit();
+					return;
+				}
+			}
+			else if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+			{
+				if (architecture == Architecture.X64)
+				{
+					GD.Print("Detected Windows on X64");
+					workingDirectory = "./win-x64-llama/";
+				}
+				else
+				{
+					GD.Print("Windows on " + architecture.ToString() + " is currently unsupported");
+					GetTree().Quit();
+					return;
+				}
+			}
+			else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+			{
+				GD.Print("Linux is currently unsupported");
+				GetTree().Quit();
+				return;
+			}
+			else if (RuntimeInformation.IsOSPlatform(OSPlatform.FreeBSD))
+			{
+				GD.Print("FreeBSD is currently unsupported");
+				GetTree().Quit();
+				return;
+			}
+			else
+			{
+				GD.Print("Your Operating System is unsupported");
+				GetTree().Quit();
+				return;
+			}
 
-            completionAddress = $"http://{host}:{port}/completion";
-        }
+			StartServer(modelPath, workingDirectory, gpuLayers, cpuThreads, maximumSessions, host, port, contextSize, maxWaitTime, allowMemoryMapping, alwaysKeepInMemory);
 
-        private bool StartServer(string modelPath, string workingDirectory, int gpuLayers, int cpuThreads, int maximumSessions, string host, short port, int contextSize, int maxWaitTime, bool allowMemoryMapping, bool alwaysKeepInMemory)
-        {
-            LLaMAProcess = new();
-            LLaMAProcess.StartInfo.ErrorDialog = false;
-            LLaMAProcess.StartInfo.UseShellExecute = false;
-            LLaMAProcess.EnableRaisingEvents = true;
-            LLaMAProcess.StartInfo.CreateNoWindow = true;
-            LLaMAProcess.StartInfo.RedirectStandardError = true;
-            LLaMAProcess.StartInfo.RedirectStandardInput = true;
-            LLaMAProcess.StartInfo.RedirectStandardOutput = true;
-            LLaMAProcess.StartInfo.WorkingDirectory = workingDirectory;
-            LLaMAProcess.StartInfo.FileName = workingDirectory + "server";
-            LLaMAProcess.StartInfo.Arguments = $"-m \"{modelPath}\" --n-gpu-layers {gpuLayers} -t {cpuThreads} --host \"{host}\" --port {port} -c {contextSize} --timeout {maxWaitTime} {(allowMemoryMapping ? "" : "--no-mmap")} {(alwaysKeepInMemory ? "--mlock" : "")} --parallel {maximumSessions} ";
-            LLaMAProcess.Exited += processExited;
-            LLaMAProcess.ErrorDataReceived += processErrorDataReceived;
-            LLaMAProcess.OutputDataReceived += processOutputDataReceived;
+			completionAddress = $"http://{host}:{port}/completion";
+		}
 
-            if (!LLaMAProcess.Start())
-            {
-                GD.Print("Could not start the AI process.");
-                return false;
-            }
+		private bool StartServer(string modelPath, string workingDirectory, int gpuLayers, int cpuThreads, int maximumSessions, string host, short port, int contextSize, int maxWaitTime, bool allowMemoryMapping, bool alwaysKeepInMemory)
+		{
+			LLaMAProcess = new();
+			LLaMAProcess.StartInfo.ErrorDialog = false;
+			LLaMAProcess.StartInfo.UseShellExecute = false;
+			LLaMAProcess.EnableRaisingEvents = true;
+			LLaMAProcess.StartInfo.CreateNoWindow = true;
+			LLaMAProcess.StartInfo.RedirectStandardError = true;
+			LLaMAProcess.StartInfo.RedirectStandardInput = true;
+			LLaMAProcess.StartInfo.RedirectStandardOutput = true;
+			LLaMAProcess.StartInfo.WorkingDirectory = workingDirectory;
+			LLaMAProcess.StartInfo.FileName = workingDirectory + "server";
+			LLaMAProcess.StartInfo.Arguments = $"-m \"{modelPath}\" --n-gpu-layers {gpuLayers} -t {cpuThreads} --host \"{host}\" --port {port} -c {contextSize} --timeout {maxWaitTime} {(allowMemoryMapping ? "" : "--no-mmap")} {(alwaysKeepInMemory ? "--mlock" : "")} --parallel {maximumSessions} ";
+			LLaMAProcess.Exited += processExited;
+			LLaMAProcess.ErrorDataReceived += processErrorDataReceived;
+			LLaMAProcess.OutputDataReceived += processOutputDataReceived;
 
-            LLaMAProcess.BeginErrorReadLine();
-            LLaMAProcess.BeginOutputReadLine();
+			if (!LLaMAProcess.Start())
+			{
+				GD.Print("Could not start the AI process.");
+				return false;
+			}
 
-            void processExited(object sender, EventArgs e)
-            {
-                GD.PrintRaw(LLaMAProcess.ExitCode + System.Environment.NewLine);
-            }
+			LLaMAProcess.BeginErrorReadLine();
+			LLaMAProcess.BeginOutputReadLine();
 
-            void processErrorDataReceived(object sender, DataReceivedEventArgs e)
-            {
-                GD.Print(e.Data);
-            }
+			void processExited(object sender, EventArgs e)
+			{
+				GD.PrintRaw(LLaMAProcess.ExitCode + System.Environment.NewLine);
+			}
 
-            void processOutputDataReceived(object sender, DataReceivedEventArgs e)
-            {
-                GD.Print(e.Data);
-            }
+			void processErrorDataReceived(object sender, DataReceivedEventArgs e)
+			{
+				GD.Print(e.Data);
+			}
 
-            return true;
-        }
-    }
+			void processOutputDataReceived(object sender, DataReceivedEventArgs e)
+			{
+				GD.Print(e.Data);
+			}
 
-    public class Session
-    {
-        public StringBuilder fullPrompt;
-        public string aiCharacterName;
-        public string playerCharacterName;
-    }
+			return true;
+		}
+	}
+
+	public class Session
+	{
+		public StringBuilder fullPrompt;
+		public string aiCharacterName;
+		public string playerCharacterName;
+	}
 
 }
 
